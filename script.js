99e55b2304ee84d5b948dd5079bc3784acb3f571
Moath100 <oufimr@icloud.com>
2025-07-13 08:57:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function() {
-    const button = document.getElementById("ChangeColorBtn");
-    button.addEventListener("click",function() {
-        const colors = ["#15,15,15", "#ffffff"];
-        const randomcolor = colors[Math.floor(Math.random()* colors.length)]; 
-        document.body.style.background = randomcolor;
-    });
-});
-    
